b3a7c876adca30567378288b109e1c95db255254
mmozammel99 <mhfaryanemon@gmail.com>
2022-08-23 00:12:46 +0600

    player list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 const playerArray = [];
 // selection list 
+function displayPlayer(player) {
+    const listFild = document.getElementById('list-fild');
+    listFild.innerHTML = "";
+    
+    for (let i = 0; i < player.length; i++) {
+        if (i < 5) {
+           
+            const name = playerArray[i];
+            const list = document.createElement("tr");
+            list.innerHTML = `
+                               <th >${i + 1}&emsp; </th>
+                                   <td >${name}</td> `
+            listFild.appendChild(list)
+            
+        }
 
+
+    }
+
+}
 // selected number section
 function addToCard(player) {
     const playerName = player.parentNode.parentNode.children[1].children[0].innerText;
